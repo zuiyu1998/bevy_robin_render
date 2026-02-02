03049068f9ecb4dd177d3fa961a86eef9c47f125
zuiyu1998 <1542844298@qq.com>
2026-02-02 09:26:59 +0800

    feat: add frame_graph

---
+++ crates/robin_graphics/src/frame_graph/mod.rs
@@ -0,0 +1,17 @@
+mod graph;
+mod index_handle;
+mod pass;
+mod pass_node;
+mod pipeline_container;
+mod resource_node;
+mod resource_table;
+mod transient_resource;
+
+pub use graph::*;
+pub use index_handle::*;
+pub use pass::*;
+pub use pass_node::*;
+pub use pipeline_container::*;
+pub use resource_node::*;
+pub use resource_table::*;
+pub use transient_resource::*;
--- crates/robin_graphics/src/lib.rs
+++ crates/robin_graphics/src/lib.rs
@@ -1,3 +1,9 @@
+extern crate alloc;
+
+mod frame_graph;
+
+pub use frame_graph::*;
+
 pub fn add(left: u64, right: u64) -> u64 {
     left + right
 }
