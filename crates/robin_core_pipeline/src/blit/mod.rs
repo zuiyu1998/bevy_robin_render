024e3262baf7f847a16d5a81b911ec082693fb9b
zuiyu1998 <1542844298@qq.com>
2026-04-27 09:28:57 +0800

    feat: BlitPlugin

---
+++ crates/robin_core_pipeline/src/blit/mod.rs
@@ -0,0 +1,115 @@
+use bevy_app::{App, Plugin};
+use bevy_asset::{AssetServer, Handle, embedded_asset, load_embedded_asset};
+use bevy_camera::CompositingSpace;
+use bevy_ecs::{
+    resource::Resource,
+    system::{Commands, Res},
+};
+use bevy_shader::Shader;
+use bevy_utils::default;
+use robin_render::{
+    GpuResourceAppExt, RenderApp, RenderStartup,
+    render_resource::{
+        binding_types::{sampler, texture_2d},
+        *,
+    },
+    renderer::RenderDevice,
+};
+
+use crate::fullscreen_vertex_shader::FullscreenShader;
+
+pub struct BlitPlugin;
+
+impl Plugin for BlitPlugin {
+    fn build(&self, app: &mut App) {
+        embedded_asset!(app, "blit.wgsl");
+
+        let Some(render_app) = app.get_sub_app_mut(RenderApp) else {
+            return;
+        };
+
+        render_app
+            .allow_ambiguous_resource::<SpecializedRenderPipelines<BlitPipeline>>()
+            .init_gpu_resource::<SpecializedRenderPipelines<BlitPipeline>>()
+            .add_systems(RenderStartup, init_blit_pipeline);
+    }
+}
+
+#[derive(Resource)]
+pub struct BlitPipeline {
+    pub layout: BindGroupLayoutDescriptor,
+    pub sampler: Sampler,
+    pub fullscreen_shader: FullscreenShader,
+    pub fragment_shader: Handle<Shader>,
+}
+
+pub fn init_blit_pipeline(
+    mut commands: Commands,
+    render_device: Res<RenderDevice>,
+    fullscreen_shader: Res<FullscreenShader>,
+    asset_server: Res<AssetServer>,
+) {
+    let layout = BindGroupLayoutDescriptor::new(
+        "blit_bind_group_layout",
+        &BindGroupLayoutEntries::sequential(
+            ShaderStages::FRAGMENT,
+            (
+                texture_2d(TextureSampleType::Float { filterable: false }),
+                sampler(SamplerBindingType::NonFiltering),
+            ),
+        ),
+    );
+
+    let sampler = render_device.create_sampler(&SamplerDescriptor::default());
+
+    commands.insert_resource(BlitPipeline {
+        layout,
+        sampler,
+        fullscreen_shader: fullscreen_shader.clone(),
+        fragment_shader: load_embedded_asset!(asset_server.as_ref(), "blit.wgsl"),
+    });
+}
+
+#[derive(PartialEq, Eq, Hash, Clone, Copy)]
+pub struct BlitPipelineKey {
+    pub target_format: TextureFormat,
+    pub blend_state: Option<BlendState>,
+    pub samples: u32,
+    /// Color space of the source texture. When `Some(Srgb)` or `Some(Oklab)`, the blit converts
+    /// to linear RGB before writing to the output target.
+    pub source_space: Option<CompositingSpace>,
+}
+
+impl SpecializedRenderPipeline for BlitPipeline {
+    type Key = BlitPipelineKey;
+
+    fn specialize(&self, key: Self::Key) -> RenderPipelineDescriptor {
+        let mut shader_defs = Vec::new();
+        match key.source_space {
+            Some(CompositingSpace::Srgb) => shader_defs.push("SRGB_TO_LINEAR".into()),
+            Some(CompositingSpace::Oklab) => shader_defs.push("OKLAB_TO_LINEAR".into()),
+            Some(CompositingSpace::Linear) | None => {}
+        }
+
+        RenderPipelineDescriptor {
+            label: Some("blit pipeline".into()),
+            layout: vec![self.layout.clone()],
+            vertex: self.fullscreen_shader.to_vertex_state(),
+            fragment: Some(FragmentState {
+                shader: self.fragment_shader.clone(),
+                shader_defs,
+                targets: vec![Some(ColorTargetState {
+                    format: key.target_format,
+                    blend: key.blend_state,
+                    write_mask: ColorWrites::ALL,
+                })],
+                ..default()
+            }),
+            multisample: MultisampleState {
+                count: key.samples,
+                ..default()
+            },
+            ..default()
+        }
+    }
+}
--- crates/robin_core_pipeline/src/lib.rs
+++ crates/robin_core_pipeline/src/lib.rs
@@ -1,15 +1,17 @@
+pub mod blit;
 pub mod core_2d;
+pub mod fullscreen_vertex_shader;
 pub mod upscaling;
 
-pub use core_2d::*;
-
 use bevy_app::{App, Plugin};
+
+use crate::{blit::BlitPlugin, core_2d::Core2dPlugin};
 
 #[derive(Default)]
 pub struct CorePipelinePlugin;
 
 impl Plugin for CorePipelinePlugin {
     fn build(&self, app: &mut App) {
-        app.add_plugins(Core2dPlugin);
+        app.add_plugins(Core2dPlugin).add_plugins(BlitPlugin);
     }
 }
